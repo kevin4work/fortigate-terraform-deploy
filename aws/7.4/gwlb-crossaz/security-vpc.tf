36186495815fdd81cfed5e901d4d03eea373feed
Kevin@Fortinet <kevinfortinet@Kevins-MacBook-Air.local>
2024-01-31 16:40:39 +0800

    add 7.4 version

---
+++ aws/7.4/gwlb-crossaz/security-vpc.tf
@@ -0,0 +1,266 @@
+// AWS VPC - Security 
+resource "aws_vpc" "fgtvm-vpc" {
+  cidr_block           = var.vpccidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  enable_classiclink   = false
+  instance_tenancy     = "default"
+  tags = {
+    Name = "terraform fgt demo"
+  }
+}
+
+resource "aws_subnet" "publicsubnetaz1" {
+  vpc_id            = aws_vpc.fgtvm-vpc.id
+  cidr_block        = var.publiccidraz1
+  availability_zone = var.az1
+  tags = {
+    Name = "public subnet az1"
+  }
+}
+
+resource "aws_subnet" "privatesubnetaz1" {
+  vpc_id            = aws_vpc.fgtvm-vpc.id
+  cidr_block        = var.privatecidraz1
+  availability_zone = var.az1
+  tags = {
+    Name = "private subnet az1"
+  }
+}
+
+resource "aws_subnet" "publicsubnetaz2" {
+  vpc_id            = aws_vpc.fgtvm-vpc.id
+  cidr_block        = var.publiccidraz2
+  availability_zone = var.az2
+  tags = {
+    Name = "public subnet az2"
+  }
+}
+
+resource "aws_subnet" "privatesubnetaz2" {
+  vpc_id            = aws_vpc.fgtvm-vpc.id
+  cidr_block        = var.privatecidraz2
+  availability_zone = var.az2
+  tags = {
+    Name = "private subnet az2"
+  }
+}
+
+// Creating Internet Gateway
+resource "aws_internet_gateway" "fgtvmigw" {
+  vpc_id = aws_vpc.fgtvm-vpc.id
+  tags = {
+    Name = "fgtvm-igw"
+  }
+}
+
+
+// Route Table
+resource "aws_route_table" "fgtvmpublicrt" {
+  vpc_id = aws_vpc.fgtvm-vpc.id
+
+  tags = {
+    Name = "fgtvm-public-rt"
+  }
+}
+
+
+resource "aws_route_table" "fgtvmprivatert" {
+  vpc_id = aws_vpc.fgtvm-vpc.id
+
+  tags = {
+    Name = "fgtvm-private-rt"
+  }
+}
+
+resource "aws_route_table" "fgtvmprivatert2" {
+  vpc_id = aws_vpc.fgtvm-vpc.id
+
+  tags = {
+    Name = "fgtvm-private-rt2"
+  }
+}
+
+resource "aws_route" "externalroute" {
+  route_table_id         = aws_route_table.fgtvmpublicrt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.fgtvmigw.id
+}
+
+resource "aws_route" "internalroute" {
+  depends_on             = [aws_instance.fgtvm]
+  route_table_id         = aws_route_table.fgtvmprivatert.id
+  destination_cidr_block = "0.0.0.0/0"
+  network_interface_id   = aws_network_interface.eth1.id
+}
+
+resource "aws_route_table_association" "public1associate" {
+  subnet_id      = aws_subnet.publicsubnetaz1.id
+  route_table_id = aws_route_table.fgtvmpublicrt.id
+}
+
+resource "aws_route_table_association" "public2associate" {
+  subnet_id      = aws_subnet.publicsubnetaz2.id
+  route_table_id = aws_route_table.fgtvmpublicrt.id
+}
+
+resource "aws_route_table_association" "internalassociate" {
+  subnet_id      = aws_subnet.privatesubnetaz1.id
+  route_table_id = aws_route_table.fgtvmprivatert.id
+}
+
+resource "aws_route_table_association" "internal2associate" {
+  subnet_id      = aws_subnet.privatesubnetaz2.id
+  route_table_id = aws_route_table.fgtvmprivatert2.id
+}
+
+resource "aws_route" "internalroute2" {
+  depends_on             = [aws_instance.fgtvm]
+  route_table_id         = aws_route_table.fgtvmprivatert2.id
+  destination_cidr_block = "0.0.0.0/0"
+  network_interface_id   = aws_network_interface.eth1-1.id
+}
+
+
+resource "aws_eip" "FGTPublicIP" {
+  depends_on        = [aws_instance.fgtvm]
+  vpc               = true
+  network_interface = aws_network_interface.eth0.id
+}
+
+resource "aws_eip" "FGTPublicIP2" {
+  depends_on        = [aws_instance.fgtvm2]
+  vpc               = true
+  network_interface = aws_network_interface.eth0-1.id
+}
+
+
+
+// Security Group
+
+resource "aws_security_group" "public_allow" {
+  name        = "Public Allow"
+  description = "Public Allow traffic"
+  vpc_id      = aws_vpc.fgtvm-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "6"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "6"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8443
+    to_port     = 8443
+    protocol    = "6"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Public Allow"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "Allow All"
+  description = "Allow all traffic"
+  vpc_id      = aws_vpc.fgtvm-vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Public Allow"
+  }
+}
+
+
+//  Gateway Load Balancer on FGT VPC to two FGTs
+resource "aws_lb" "gateway_lb" {
+  name                             = "gatewaylb"
+  load_balancer_type               = "gateway"
+  enable_cross_zone_load_balancing = "true"
+
+  subnet_mapping {
+    subnet_id = aws_subnet.privatesubnetaz1.id
+  }
+
+  subnet_mapping {
+    subnet_id = aws_subnet.privatesubnetaz2.id
+  }
+
+  tags = {
+    Environment = "terraform sample"
+  }
+}
+
+
+resource "aws_lb_target_group" "fgt_target" {
+  name        = "fgttarget"
+  port        = 6081
+  protocol    = "GENEVE"
+  target_type = "ip"
+  vpc_id      = aws_vpc.fgtvm-vpc.id
+
+  health_check {
+    port     = 8008
+    protocol = "TCP"
+  }
+}
+
+resource "aws_lb_listener" "fgt_listener" {
+  load_balancer_arn = aws_lb.gateway_lb.id
+
+  default_action {
+    target_group_arn = aws_lb_target_group.fgt_target.id
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "fgtattach" {
+  depends_on       = [aws_instance.fgtvm]
+  target_group_arn = aws_lb_target_group.fgt_target.arn
+  target_id        = data.aws_network_interface.eth1.private_ip
+  port             = 6081
+}
+resource "aws_lb_target_group_attachment" "fgtattach2" {
+  depends_on       = [aws_instance.fgtvm2]
+  target_group_arn = aws_lb_target_group.fgt_target.arn
+  target_id        = data.aws_network_interface.eth1-1.private_ip
+  port             = 6081
+}
+
+
+
+resource "aws_vpc_endpoint_service" "fgtgwlbservice" {
+  acceptance_required        = false
+  gateway_load_balancer_arns = [aws_lb.gateway_lb.arn]
+}
+
---
+++ aws/7.4/gwlb-crossaz/variables.tf
@@ -0,0 +1,398 @@
+//AWS Configuration
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "region" {
+  default = "eu-west-1"
+}
+
+// Availability zones for the region
+variable "az1" {
+  default = "eu-west-1a"
+}
+
+variable "az2" {
+  default = "eu-west-1b"
+}
+
+// VPC for FortiGate Security VPC
+variable "vpccidr" {
+  default = "10.1.0.0/16"
+}
+
+variable "publiccidraz1" {
+  default = "10.1.0.0/24"
+}
+
+variable "privatecidraz1" {
+  default = "10.1.1.0/24"
+}
+
+variable "publiccidraz2" {
+  default = "10.1.2.0/24"
+}
+
+variable "privatecidraz2" {
+  default = "10.1.3.0/24"
+}
+
+// VPC for Customer VPC
+variable "csvpccidr" {
+  default = "20.1.0.0/16"
+}
+
+variable "cspubliccidraz1" {
+  default = "20.1.0.0/24"
+}
+
+variable "csprivatecidraz1" {
+  default = "20.1.1.0/24"
+}
+
+
+variable "cspubliccidraz2" {
+  default = "20.1.2.0/24"
+}
+
+variable "csprivatecidraz2" {
+  default = "20.1.3.0/24"
+}
+
+
+
+// License Type to create FortiGate-VM
+// Provide the license type for FortiGate-VM Instances, either byol or payg.
+variable "license_type" {
+  default = "payg"
+}
+
+// instance architect
+// Either arm or x86
+variable "arch" {
+  default = "arm"
+}
+
+// instance type needs to match the architect
+// c5.xlarge is x86_64
+// c6g.xlarge is arm
+// For detail, refer to https://aws.amazon.com/ec2/instance-types/
+variable "size" {
+  default = "c6g.xlarge"
+}
+
+// AMIs for FGTVM-7.4.2
+variable "fgtami" {
+  type = map(any)
+  default = {
+    us-east-1 = {
+      arm = {
+        payg = "ami-053c935b698f02899"
+        byol = "ami-04ce66e78ff5a4ef6"
+      },
+      x86 = {
+        payg = "ami-069777173bc107452"
+        byol = "ami-023b1987e16a18b60"
+      }
+    },
+    us-east-2 = {
+      arm = {
+        payg = "ami-0334d602c0ed7d0b1"
+        byol = "ami-0a1e00c98ae19897c"
+      },
+      x86 = {
+        payg = "ami-0599d618948826eb2"
+        byol = "ami-025c7ab2a8a1dfa49"
+      }
+    },
+    us-west-1 = {
+      arm = {
+        payg = "ami-09bd9c33d04bcdf4a"
+        byol = "ami-00c571443372aa6a8"
+      },
+      x86 = {
+        payg = "ami-0153df6ede9484eb2"
+        byol = "ami-0124eb8ef6e5c283a"
+      }
+    },
+    us-west-2 = {
+      arm = {
+        payg = "ami-03344bf5eb5fc9e0a"
+        byol = "ami-0e3a7847a9aa624eb"
+      },
+      x86 = {
+        payg = "ami-0a62643a3c57dacb3"
+        byol = "ami-0d777364299515223"
+      }
+    },
+    af-south-1 = {
+      arm = {
+        payg = "ami-01cd00b91977299bb"
+        byol = "ami-0b1c5f6533484a70c"
+      },
+      x86 = {
+        payg = "ami-07cfbecd499c1e704"
+        byol = "ami-0f97cab8731ce41f3"
+      }
+    },
+    ap-east-1 = {
+      arm = {
+        payg = "ami-0a025e35db43b00f1"
+        byol = "ami-04a7bc962fdd19bbf"
+      },
+      x86 = {
+        payg = "ami-0acd179b8cb3f6b22"
+        byol = "ami-0afa71336fdf0e681"
+      }
+    },
+    ap-south-2 = {
+      arm = {
+        payg = "ami-00d27eba74ab90870"
+        byol = "ami-0bdce6887321db07f"
+      },
+      x86 = {
+        payg = "ami-0a419bb20bd64aceb"
+        byol = "ami-0b08d78d60fa4cd71"
+      }
+    },
+    ap-southeast-3 = {
+      arm = {
+        payg = "ami-0aa106972a8b34a46"
+        byol = "ami-01830e93b72325481"
+      },
+      x86 = {
+        payg = "ami-0fe1f21464892deef"
+        byol = "ami-0823a3569f30af8f8"
+      }
+    },
+    ap-south-4 = {
+      arm = {
+        payg = "ami-0af4f1b3e1b1044cb"
+        byol = "ami-0cbcf8f2ad29b2de8"
+      },
+      x86 = {
+        payg = "ami-02c59ea0f1fdd9d3e"
+        byol = "ami-0920a1b3d41bc8f71"
+      }
+    },
+    ap-south-1 = {
+      arm = {
+        payg = "ami-00e831ee476364b78"
+        byol = "ami-09df150b12917a755"
+      },
+      x86 = {
+        payg = "ami-034c9d5ff899d3f2a"
+        byol = "ami-0afcb1664e5e8c725"
+      }
+    },
+    ap-northeast-3 = {
+      arm = {
+        payg = "ami-0a90188f24033ae33"
+        byol = "ami-08128b71709e6992f"
+      },
+      x86 = {
+        payg = "ami-0fffb4a7f1c1ca9d6"
+        byol = "ami-0acc5c073ded421e6"
+      }
+    },
+    ap-northeast-2 = {
+      arm = {
+        payg = "ami-0dec8836cbc591c10"
+        byol = "ami-099aaf5111a810d59"
+      },
+      x86 = {
+        payg = "ami-0c68a68a3575014ad"
+        byol = "ami-0ab05da220c15bcf0"
+      }
+    },
+    ap-southeast-1 = {
+      arm = {
+        payg = "ami-09bf4f13a0a48a9e9"
+        byol = "ami-0b712c3adff6167d3"
+      },
+      x86 = {
+        payg = "ami-0d4e0ff377dd74db3"
+        byol = "ami-0c3f47755ba6939f2"
+      }
+    },
+    ap-southeast-2 = {
+      arm = {
+        payg = "ami-01d26e7f4fb9959cb"
+        byol = "ami-0984b76f236478424"
+      },
+      x86 = {
+        payg = "ami-09629d072489a5772"
+        byol = "ami-0d31fcf38f166e16d"
+      }
+    },
+    ap-northeast-1 = {
+      arm = {
+        payg = "ami-0cfc9ce281bd6be3f"
+        byol = "ami-0884d6ac05818ebe7"
+      },
+      x86 = {
+        payg = "ami-09172aeaf9b4df8c1"
+        byol = "ami-018f653c813584b08"
+      }
+    },
+    ca-central-1 = {
+      arm = {
+        payg = "ami-04bb2460da5872b09"
+        byol = "ami-0a2849369784ec679"
+      },
+      x86 = {
+        payg = "ami-0e04ba5054ca4da5b"
+        byol = "ami-0cd2ce40017e2f5a2"
+      }
+    },
+    eu-central-1 = {
+      arm = {
+        payg = "ami-09f5d58b1871ec3ec"
+        byol = "ami-06cdbd403c84d9149"
+      },
+      x86 = {
+        payg = "ami-01bd410bcaa617f44"
+        byol = "ami-006569d5876e72b08"
+      }
+    },
+    eu-west-1 = {
+      arm = {
+        payg = "ami-0e7e5bcec5354aa8b"
+        byol = "ami-09d8f034dadcd3df1"
+      },
+      x86 = {
+        payg = "ami-0dd7de3ea96419dbd"
+        byol = "ami-06b664d1b3aca5a87"
+      }
+    },
+    eu-west-2 = {
+      arm = {
+        payg = "ami-0e17ce19a0e15caa5"
+        byol = "ami-0a43904906c39bb05"
+      },
+      x86 = {
+        payg = "ami-03471a82df55ecce4"
+        byol = "ami-0b8481a08fe6afec4"
+      }
+    },
+    eu-south-1 = {
+      arm = {
+        payg = "ami-0e8cba4ab3b5144de"
+        byol = "ami-0a3088f8ae59f7b28"
+      },
+      x86 = {
+        payg = "ami-0b0d88d7406eb6435"
+        byol = "ami-0aec89d42c6e6e761"
+      }
+    },
+    eu-west-3 = {
+      arm = {
+        payg = "ami-069b42fb2a9307a7f"
+        byol = "ami-07dc962d73c169e1f"
+      },
+      x86 = {
+        payg = "ami-04fb7eaa45bcdf668"
+        byol = "ami-05e0c4faa4d24ab58"
+      }
+    },
+    eu-south-2 = {
+      arm = {
+        payg = "ami-092e8c8ba847d59ae"
+        byol = "ami-046525f54a600dce5"
+      },
+      x86 = {
+        payg = "ami-0d13fadc5438f6201"
+        byol = "ami-097a6056577d66e68"
+      }
+    },
+    eu-north-1 = {
+      arm = {
+        payg = "ami-0c5b00356602179b1"
+        byol = "ami-034bd7dabae6b987b"
+      },
+      x86 = {
+        payg = "ami-026697e3f2e27cc14"
+        byol = "ami-0eca84214d4cf6895"
+      }
+    },
+    eu-central-2 = {
+      arm = {
+        payg = "ami-0a9f05d091fc1c25f"
+        byol = "ami-059f3eba3e7c63246"
+      },
+      x86 = {
+        payg = "ami-0f48420bee09cf6a7"
+        byol = "ami-0b90d9316ce0daab9"
+      }
+    },
+    me-south-1 = {
+      arm = {
+        payg = "ami-00f2fa916cb024dcd"
+        byol = "ami-00c00a9179f50cf0b"
+      },
+      x86 = {
+        payg = "ami-0b5730dc28f6ded0c"
+        byol = "ami-040739f3f9479feb8"
+      }
+    },
+    me-central-1 = {
+      arm = {
+        payg = "ami-0acc7c90b713c52c5"
+        byol = "ami-03444391e8842e6b2"
+      },
+      x86 = {
+        payg = "ami-0382510c43e5299f5"
+        byol = "ami-08c67d0c55223ebf9"
+      }
+    },
+    il-central-1 = {
+      arm = {
+        payg = "ami-0afa7664edfe8b52c"
+        byol = "ami-04feba963372af61e"
+      },
+      x86 = {
+        payg = "ami-08af016469c9be301"
+        byol = "ami-085188e1ddbe43125"
+      }
+    },
+    sa-east-1 = {
+      arm = {
+        payg = "ami-0d974801fcb555564"
+        byol = "ami-065d91967c9bd6a5d"
+      },
+      x86 = {
+        payg = "ami-097f184146630b559"
+        byol = "ami-0f64e989009f155f4"
+      }
+    }
+  }
+}
+
+//  Existing SSH Key on the AWS 
+variable "keyname" {
+  default = "<AWS SSH KEY>"
+}
+
+//  Admin HTTPS access port
+variable "adminsport" {
+  default = "443"
+}
+
+variable "bootstrap-fgtvm" {
+  // Change to your own path
+  type    = string
+  default = "fgtvm.conf"
+}
+
+// license token for the fgt
+variable "license" {
+  // Change to your own token 2
+  type    = string
+  default = "xxx"
+}
+
+// license token for the fgt
+variable "license2" {
+  // Change to your own token 2
+  type    = string
+  default = "xxx"
+}
